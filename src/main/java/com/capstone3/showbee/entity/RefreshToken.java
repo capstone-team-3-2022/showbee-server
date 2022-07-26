e38c58b1ef2a2ab60f753c118a355f8e6b21cdfb
하지현 <kkim.haji@gmail.com>
2022-07-26 19:52:37 +0900

    conflict

--- src/main/java/com/capstone3/showbee/entity/RefreshToken.java
+++ src/main/java/com/capstone3/showbee/entity/RefreshToken.java
@@ -14,7 +14,6 @@ import java.util.StringTokenizer;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@AllArgsConstructor
 public class RefreshToken {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long rid;
